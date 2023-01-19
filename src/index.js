79c72029910384b6a83979313acfa42bfa5fe658
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-19 08:48:45 -0800

    Add item forms

--- src/index.js
+++ src/index.js
@@ -8,7 +8,62 @@ class todoItem {
     };
 };
 
+class todoList {
+    constructor() {
+        this.allTodoItems = [];
+    };
+
+    insertTodo(item) {
+        this.allTodoItems.push(item);
+    };
+
+    sortList() {
+        console.log("sort");
+    };
+};
+
+function displayList(lst) {
+
+};
+
+theList = new todoList();
+
 const addTodo = document.getElementById("add-button");
 addTodo.addEventListener("click", () => {
-    console.log("event listener test");
+    document.getElementById("blank").style.height = "100vh";
+    document.getElementById("popupForm").style.display = "block";
 });
+
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if ((document.getElementById("title").value == '') ||  (document.getElementById("due-date").value == '')) {
+        return alert('Please include todo title and due date');
+    };
+    let newProject = document.getElementById("project").value;
+    document.getElementById("project").value = '';
+    let newTitle = document.getElementById("title").value;
+    document.getElementById("title").value = '';
+    let newDescription = +document.getElementById("description").value;
+    document.getElementById("description").value = '';
+    let newDueDate = document.getElementById("due-date").value;
+    document.getElementById("due-date").value = '';
+    let newPriority = document.querySelector("#priority").checked;
+    document.querySelector("#priority").checked = false;
+    let newTodo = new todoItem(newProject, newTitle, newDescription, newDueDate, newPriority);
+    theList.insertTodo(newTodo);
+    console.log(theList);
+    document.getElementById("blank").style.height = "0vh";
+    document.getElementById("popupForm").style.display = "none";
+});
+
+const cancelButton = document.getElementById("cancel-button");
+cancelButton.addEventListener("click", () => {
+    document.getElementById("project").value = '';
+    document.getElementById("title").value = '';
+    document.getElementById("description").value = '';
+    document.getElementById("due-date").value = '';
+    document.querySelector("#priority").checked = false;
+    document.getElementById("blank").style.height = "0vh";
+    document.getElementById("popupForm").style.display = "none";
+  });
